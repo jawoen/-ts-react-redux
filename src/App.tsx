5091ae9a14be43705d9a25fe1c38fcefa558afb8
sdrjseka9 <sdrjseka9@naver.com>
2023-02-24 15:52:59 +0900

    0224

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CounterContainer from './containers/CounterContainer';
+import TodosContainer from './containers/TodosContainer';
+import GithubContainer from './containers/GithubContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CounterContainer/>
+      <TodosContainer/>
+      <GithubContainer/>
     </div>
   );
 }
